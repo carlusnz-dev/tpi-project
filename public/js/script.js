5941447d323a47bd2d0e6815385727744f8d29cd
Carlos Antunes <carlosantunes.dev@gmail.com>
2023-10-20 10:19:48 -0300

    adição do footer e pagina project

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,13 @@
 // navbar scrolled
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+})
+
 document.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 50) {
@@ -14,4 +23,14 @@ navLinks.forEach(link => {
     if (link.href === window.location.href) {
         link.classList.add("active");
     }
+})
+
+// mudar para o modo escuro e claro bootstrap 5
+const btnSwitch = document.querySelector(".btn-switch");
+const btnSwitchSun = document.querySelector(".bi-sun");
+const btnSwitchMoon = document.querySelector(".bi-moon");
+btnSwitch.addEventListener("click", () => {
+    btnSwitchSun.classList.toggle("d-none");
+    btnSwitchMoon.classList.toggle("d-none");
+    document.body.classList.toggle("dark-mode");
 })
