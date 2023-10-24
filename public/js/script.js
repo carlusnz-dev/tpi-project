531055a9f76b06ee254b3f8be3c9d164e6604cf8
Carlos Antunes <carlosantunes.dev@gmail.com>
2023-10-24 15:39:02 -0300

    atualizacao da barra de pesquisa

--- public/js/script.js
+++ public/js/script.js
@@ -35,3 +35,47 @@ btnSwitch.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 })
 
+document.addEventListener("DOMContentLoaded", () => {
+    // Formulário de pesquisa
+    const searchForm = document.querySelector(".search-form");
+    searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const queryInput = searchForm.querySelector("input[name='query']");
+        const query = queryInput.value;
+        window.location.href = `element.html?atomicNumber=${query}`;
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchForm = document.querySelector(".search-form");
+    const queryInput = document.getElementById("form-control");
+
+    searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const query = queryInput.value;
+
+        // Aqui você pode verificar se a consulta é um número ou um nome
+        // Se for um número, redirecione diretamente para element.html?numeroAtomico=x
+        if (!isNaN(query)) {
+            const atomicNumber = parseInt(query, 10);
+            window.location.href = `element.html?atomicNumber=${atomicNumber}`;
+        } else {
+            // Se for um nome, você precisará buscar o JSON para obter a correspondência do nome
+            fetch("content.json")
+                .then((response) => response.json())
+                .then((data) => {
+                    const elementos = data.elementos;
+                    const elemento = elementos[query];
+
+                    if (elemento) {
+                        window.location.href = `element.html?atomicNumber=${elemento.numeroAtomico}`;
+                    } else {
+                        console.log("Elemento não encontrado.");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Erro ao buscar os dados dos elementos:", error);
+                });
+        }
+    });
+});
